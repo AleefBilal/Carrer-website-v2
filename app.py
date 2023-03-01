39b1801d21470b023dbe6029641d89982f2884c7
Aleef Bilal Bilal <90944779+AleefBilal@users.noreply.github.com>
2023-03-01 19:27:52 +0000

    Added a list

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+JOBS = [
+    {
+      'id': 1,
+      'title': 'Data Analyst',
+      'Location': 'Lahore',
+      'Salary': '150,000'
+    },
+  {
+      'id': 2,
+      'title': 'Penetration Testing',
+      'Location': 'Islamabad',
+      'Salary': '120,000'
+    },
+  {
+      'id': 3,
+      'title': 'Project Manager',
+      'Location': 'Lahore',
+      
+    },
+]
 
 
 @app.route("/")
 def hello_jovain():
-  return render_template('home.html')
+  return render_template('home.html', jobs = JOBS)
 
 
 if __name__ == '__main__':
