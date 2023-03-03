732998626b5f48c93406d31f9fd1ca90a501a006
Aleef Bilal Bilal <90944779+AleefBilal@users.noreply.github.com>
2023-03-03 11:57:40 +0000

    Added josn

--- app.py
+++ app.py
@@ -1,31 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-JOBS = [
-    {
-      'id': 1,
-      'title': 'Data Analyst',
-      'Location': 'Lahore',
-      'Salary': '150,000'
-    },
-  {
-      'id': 2,
-      'title': 'Penetration Testing',
-      'Location': 'Islamabad',
-      'Salary': '120,000'
-    },
-  {
-      'id': 3,
-      'title': 'Project Manager',
-      'Location': 'Lahore',
-      
-    },
-]
+JOBS = [{
+  'id': 1,
+  'title': 'Data Analyst',
+  'Location': 'Lahore',
+  'Salary': '150,000'
+}, {
+  'id': 2,
+  'title': 'Penetration Testing',
+  'Location': 'Islamabad',
+  'Salary': '120,000'
+}, {
+  'id': 3,
+  'title': 'UI/UX Engineer',
+  'Location': 'Jhelum',
+  'Salary': '150,000'
+}, {
+  'id': 4,
+  'title': 'Project Manager',
+  'Location': 'Lahore',
+}]
 
 
 @app.route("/")
 def hello_jovain():
-  return render_template('home.html', jobs = JOBS)
+  return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
