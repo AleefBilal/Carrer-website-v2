d2d5592fff416aa61e7684c3897a9703832ea580
Aleef Bilal Bilal <90944779+AleefBilal@users.noreply.github.com>
2023-03-29 09:44:52 +0000

    Made it completely Dynamic

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs, load_job
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs, load_job, add_app_db
 
 app = Flask(__name__)
 
@@ -24,6 +24,14 @@ def show_id(id):
     return "Not Found", 404
   else:
     return render_template('joblist.html', jobs=job)
+
+
+@app.route("/jobs/<id>/apply", methods=['post'])
+def apply_job(id):
+  data = request.form
+  job = load_job(id)
+  add_app_db(id, data)
+  return render_template('app_submit.html', application=data, job=job)
 
 
 if __name__ == '__main__':
--- database.py
+++ database.py
@@ -29,3 +29,21 @@ def load_job(id):
       return None
     else:
       return dict(zip(result.keys(), row))
+
+
+def add_app_db(job_id, data):
+  with engine.connect() as conn:
+    query = text(
+      "INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)"
+    )
+
+    conn.execute(
+      query, {
+        'job_id': job_id,
+        'full_name': data['full_name'],
+        'email': data['email'],
+        'linkedin_url': data['linkedin_url'],
+        'education': data['education'],
+        'work_experience': data['work_experience'],
+        'resume_url': data['resume_url']
+      })
